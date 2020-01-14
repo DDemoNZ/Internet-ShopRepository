7fb0934e72795225969acc202560b4a2aa22eb5e
Dmitrii <zinchukdimaa@gmail.com>
2020-01-14 11:54:53 +0200

    logger

--- src/main/java/mate/academy/internetshop/App.java
+++ src/main/java/mate/academy/internetshop/App.java
@@ -1,13 +1,23 @@
 package mate.academy.internetshop;
 
+import java.util.List;
+
 import mate.academy.internetshop.lib.Inject;
 import mate.academy.internetshop.lib.Injector;
+import mate.academy.internetshop.model.Bucket;
+import mate.academy.internetshop.model.Item;
+import mate.academy.internetshop.model.Order;
+import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.service.BucketService;
 import mate.academy.internetshop.service.ItemService;
 import mate.academy.internetshop.service.OrderService;
 import mate.academy.internetshop.service.UserService;
+import org.apache.log4j.Logger;
 
 public class App {
+
+    static final Logger logger = Logger.getLogger(App.class);
+
     @Inject
     private static ItemService itemService;
 
@@ -26,5 +36,21 @@ public class App {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        Item item1 = new Item("item1", 1.0);
+        itemService.create(item1);
+        User user1 = new User("user1");
+        userService.create(user1);
+        Bucket bucket = new Bucket(user1.getUserId());
+        bucketService.create(bucket);
+        bucketService.addItem(bucket, item1);
+        List<Item> allItems = bucketService.getAllItems(bucket);
+        Order order = orderService.completeOrder(allItems, user1);
+        System.out.println(order);
+        System.out.println(user1);
+        logger.info("INFO");
+        logger.debug("DEBUG");
     }
 }
--- src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
+++ src/main/java/mate/academy/internetshop/controller/InjectInitializer.java
@@ -4,14 +4,19 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import mate.academy.internetshop.lib.Injector;
+import org.apache.log4j.Logger;
 
 public class InjectInitializer implements ServletContextListener {
+
+    static final Logger logger = Logger.getLogger(InjectInitializer.class);
 
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         try {
+            logger.debug("InjectInitializer start");
             Injector.injectDependency();
         } catch (IllegalAccessException e) {
+            logger.error("InjectInitializer can't start");
             throw new RuntimeException(e);
         }
     }
