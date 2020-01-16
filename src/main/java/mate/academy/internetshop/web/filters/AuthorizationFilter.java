feffbb8efe55f18be94dbba9a79932fb0b72eb52
Dmitrii <zinchukdimaa@gmail.com>
2020-01-16 19:26:27 +0200

    fix checkstyle errors

--- src/main/java/mate/academy/internetshop/web/filters/AuthorizationFilter.java
+++ src/main/java/mate/academy/internetshop/web/filters/AuthorizationFilter.java
@@ -1,27 +1,24 @@
 package mate.academy.internetshop.web.filters;
 
-import mate.academy.internetshop.model.Role;
-import mate.academy.internetshop.model.User;
+import static mate.academy.internetshop.model.Role.RoleName.ADMIN;
+import static mate.academy.internetshop.model.Role.RoleName.USER;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import mate.academy.internetshop.lib.Inject;
+import mate.academy.internetshop.model.Role;
+import mate.academy.internetshop.model.User;
 import mate.academy.internetshop.service.UserService;
-
-import static mate.academy.internetshop.model.Role.RoleName.ADMIN;
-import static mate.academy.internetshop.model.Role.RoleName.USER;
 
 public class AuthorizationFilter implements Filter {
 
