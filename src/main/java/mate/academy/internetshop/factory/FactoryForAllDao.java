1f0bc4ac3e771a63166a9cef1c57bc717b75f207
Dmitrii <zinchukdimaa@gmail.com>
2020-01-22 13:16:08 +0200

    added: ItemDaoJdbcImpl and CRUD method for DB added: Driver and connection method create init_db.sql with command for create DB

--- src/main/java/mate/academy/internetshop/factory/FactoryForAllDao.java
+++ src/main/java/mate/academy/internetshop/factory/FactoryForAllDao.java
@@ -1,13 +1,17 @@
 package mate.academy.internetshop.factory;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import mate.academy.internetshop.dao.BucketDao;
 import mate.academy.internetshop.dao.ItemDao;
 import mate.academy.internetshop.dao.OrderDao;
 import mate.academy.internetshop.dao.UserDao;
 import mate.academy.internetshop.dao.impl.BucketDaoImpl;
-import mate.academy.internetshop.dao.impl.ItemDaoImpl;
 import mate.academy.internetshop.dao.impl.OrderDaoImpl;
 import mate.academy.internetshop.dao.impl.UserDaoImpl;
+import mate.academy.internetshop.dao.jdbc.ItemDaoJdbcImpl;
 import mate.academy.internetshop.service.BucketService;
 import mate.academy.internetshop.service.ItemService;
 import mate.academy.internetshop.service.OrderService;
@@ -16,6 +20,7 @@ import mate.academy.internetshop.service.impl.BucketServiceImpl;
 import mate.academy.internetshop.service.impl.ItemServiceImpl;
 import mate.academy.internetshop.service.impl.OrderServiceImpl;
 import mate.academy.internetshop.service.impl.UserServiceImpl;
+import org.apache.log4j.Logger;
 
 public class FactoryForAllDao {
 
@@ -28,12 +33,26 @@ public class FactoryForAllDao {
     private static ItemService itemService;
     private static BucketService bucketService;
 
+    private static Connection connection;
+    private static Logger logger = Logger.getLogger(FactoryForAllDao.class);
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection =
+                    DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/internetshop?"
+                            + "user=newUserDB&password=1234user&serverTimezone=UTC");
+        } catch (ClassNotFoundException | SQLException e) {
+            logger.info("Cant establish connection to DB ", e);
+        }
+    }
+
     public static BucketDao getBucketDao() {
         return bucketDao == null ? new BucketDaoImpl() : bucketDao;
     }
 
     public static ItemDao getItemDao() {
-        return itemDao == null ? new ItemDaoImpl() : itemDao;
+        return new ItemDaoJdbcImpl(connection);
     }
 
     public static OrderDao getOrderDao() {
---
+++ src/main/resources/init_db.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA `internetshop` DEFAULT CHARACTER SET utf8 ;
+CREATE TABLE `internetshop`.`items` (
+                                        `item_id` INT NOT NULL AUTO_INCREMENT,
+                                        `name` VARCHAR(225) NOT NULL,
+                                        `price` DECIMAL(6,2) NOT NULL,
+                                        PRIMARY KEY (`item_id`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8;
+INSERT INTO `internetshop`.`items` (`name`, `price`) VALUES ('item1', '100.00');
+INSERT INTO `internetshop`.`items` (`name`, `price`) VALUES ('item2', '200.00');
+
